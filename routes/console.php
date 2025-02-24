b263b978e048d86f9edfdb0c2f4e23c7cfe92fe8
a.zubkov <zubkoffalex4nder@yandex.ru>
2025-02-24 17:46:55 +0300

    remove static call

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 
-Schedule::command(ImportJokes::class)->everyFiveMinutes();
+Schedule::command('app:import-jokes')->everyFiveMinutes();
+
